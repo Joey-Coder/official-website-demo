28892f21296b43ffe2ac328812e296890fd437cc
Joey-Coder <joey950326@gmail.com>
2021-04-21 23:24:39 +0800

    压缩除assets外的js代码

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
 const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -36,6 +37,11 @@ module.exports = merge(common, {
       // 在 webpack@5 中，你可以使用 `...` 语法来扩展现有的 minimizer（即 `terser-webpack-plugin`），将下一行取消注释
       new CssMinimizerPlugin({
         parallel: true,
+      }),
+      new TerserPlugin({
+        parallel: true,
+        // 不需要压缩dist/assets里面js
+        exclude: /assets/,
       }),
     ],
   },
